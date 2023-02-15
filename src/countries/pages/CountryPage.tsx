ec6f7221b089b770d15a0fa65a69195bcf11956d
Fernando Vallejo <fervalle200190@gmail.com>
2023-02-15 12:01:43 -0400

    quick fix

--- src/countries/pages/CountryPage.tsx
+++ src/countries/pages/CountryPage.tsx
@@ -20,23 +20,26 @@ export const CountryPage = () => {
 
      const existCountry = Object.keys(activeCountry).length > 0 ? true : false
 
-     if (!existCountry) return "Loading"
+     if (!existCountry) return <>"Loading"</>
 
      const onBackButtonClick = () => {
           navigate("/")
      }
 
      return (
-          <div className={`country-page-container ${isDarkModeActive? 'bg-c-d': ''}`}>
-               <div className={`back-btn ${isDarkModeActive? 'btn-dark': ''}`} onClick={onBackButtonClick}>
+          <div className={`country-page-container ${isDarkModeActive ? "bg-c-d" : ""}`}>
+               <div
+                    className={`back-btn ${isDarkModeActive ? "btn-dark" : ""}`}
+                    onClick={onBackButtonClick}
+               >
                     <IonIcon name='arrow-back' />
                     Back
                </div>
-               <div className="country-data">
+               <div className='country-data'>
                     <div className='image-page-details'>
                          <img src={activeCountry.flags?.svg} alt={activeCountry.name?.common} />
                     </div>
-                    <div className="big-details-container">
+                    <div className='big-details-container'>
                          <CountryDetails activeCountry={activeCountry} />
                          <CountryButtonBorders />
                     </div>
